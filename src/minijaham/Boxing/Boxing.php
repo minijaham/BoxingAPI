9bdd414943fc3b5a6f1544bd059d146b280ef280
Mini S <60860894+minijaham@users.noreply.github.com>
2021-09-11 21:42:21 +0900

    Code clean

--- src/minijaham/Boxing/Boxing.php
+++ src/minijaham/Boxing/Boxing.php
@@ -17,37 +17,37 @@ class Boxing extends PluginBase implements Listener
 	public static Boxing $instance;
 	
 	public static function getInstance() : self
-    {
-        return self::$instance;
-    }
+    	{
+        	return self::$instance;
+    	}
 	
 	public function onAttack(EntityDamageEvent $event) : void
 	{
-        $entity = $event->getEntity();
+        	$entity = $event->getEntity();
 		if ($event->isCancelled()) {
 			return;
 		}
-        if (!($event instanceof EntityDamageByEntityEvent)) {
+        	if (!($event instanceof EntityDamageByEntityEvent)) {
 			return;
 		}
-        $damager = $event->getDamager();
-        if (!($entity instanceof Player)) {
+        	$damager = $event->getDamager();
+        	if (!($entity instanceof Player)) {
 			return;
 		}
-        if (!($damager instanceof Player)) {
+        	if (!($damager instanceof Player)) {
 			return;
 		}
 		if (!isset($this->hits[$damager->getName()])) {
 			$this->hits[$damager->getName()] = 1;
-            return;
+            		return;
 		}
 		$this->hits[$damager->getName()] = $this->hits[$damager->getName()]++;
-    }
+    	}
 	
 	public function getHits(Player $player) : int
 	{
 		if (!isset($this->hits[$player->getName()])) {
-            return 0;
+            		return 0;
 		}
 		return $this->hits[$player->getName()];
 	}
@@ -55,7 +55,7 @@ class Boxing extends PluginBase implements Listener
 	public function removeHits(Player $player) : void
 	{
 		if (isset($this->hits[$player->getName()])) {
-            unset($this->hits[$player->getName()]);
+            		unset($this->hits[$player->getName()]);
 			return;
 		}
 	}
